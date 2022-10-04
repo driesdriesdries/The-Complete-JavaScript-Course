1a840f0e1031e830bb7ffbad093a31ced798710f
Dries <bester.dries@gmail.com>
2022-10-04 19:15:47 +0200

    completed the assignment for lesson10

--- 01/script.js
+++ 01/script.js
@@ -1,3 +1,11 @@
-let js = 'amazing';
-if ( js === 'amazing') alert('this is amazing');
-console.log(4+4+4);
+//declaring a variable
+let country = "South Africa";
+let continent = "Africa";
+let population = "50000000";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//opening the console on Windows with chrome cntrl shift J
+//opening terminal in VS code with windows cntrl J
