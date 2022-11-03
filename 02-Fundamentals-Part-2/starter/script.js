9cbebb05c7cebb754ecd100b58c8637b6e1f0187
Dries <bester.dries@gmail.com>
2022-11-03 10:03:26 +0200

    completed the lesson on functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,11 +1,35 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive');
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive');
 
-//Reserved word
-// const interface = 'Audio';
-// const private = 's33';
+// //Reserved word
+// // const interface = 'Audio';
+// // const private = 's33';
+
+// Lesson 33
+//Functions
+
+function logger(){
+    console.log('My name is Dries');
+}
+
+//calling, running, invoking function
+logger();
+
+function fruitProcessor (apples, oranges){
+    // console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    
+    //saving the result of the function
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
